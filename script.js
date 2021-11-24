b66f4e730995d4216f90a0655808ea2f522752ab
php5art <php5art@gmail.com>
2021-11-24 18:17:49 +0300

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello!')
+var dice = document.querySelector('.dice');
+var radioGroup = document.querySelector('.radio-group');
+var currentClass = '';
+
+function changeSide() {
+  var checkedRadio = radioGroup.querySelector(':checked');
+  var showClass = 'show-' + checkedRadio.value;
+  if (currentClass) {
+    dice.classList.remove(currentClass);
+  }
+  dice.classList.add(showClass);
+  currentClass = showClass;
+}
+// set initial side
+changeSide();
+
+radioGroup.addEventListener('change', changeSide);
